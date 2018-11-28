30311b9cfcaf178535a79cda29d8322347bb2919
杨振华 <yangzhenhua@bizvane.cn>
2018-11-28 14:35:44 +0800

    注册中心初始化

---
+++ shop-eureka-server/src/main/java/com/shop/eureka/ShopEurekaServer.java
@@ -0,0 +1,14 @@
+package com.shop.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ShopEurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEurekaServer.class,args);
+    }
+}
